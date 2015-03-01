56e946fb1369cff01b61c355039aba088e9edca6
Jacob Joseph <jjoseph03@mica.edu>
2015-03-01 09:02:21 -0800

    switched from linear to tabular dataset, began creating stacked bar chart and datafilters with d3 in main.js

--- js/main.js
+++ js/main.js
@@ -17,11 +17,14 @@ var x = d3.scale.linear()
 var y = d3.scale.ordinal()
 	.rangeBands([0, height], 0.2, 0);
 
+var color = d3.scale.ordinal()
+	.range(["#EEE", "#BDF", "#FCF", "#EEE"]);
+
 var xAxis = d3.svg.axis()
 	.scale(x)
 	.orient("bottom")
-	.ticks(10, "%")
-	.tickSize(-height);
+	.ticks(10, "%") // Not sure if this is right since it is 50/50 split NEEDFIX
+	.tickSize(-height); // No clue where this is going to end up... NEEDFIX
 
 var yAxis = d3.svg.axis()
 	.scale(y)
@@ -29,7 +32,7 @@ var yAxis = d3.svg.axis()
 
 var playAll = OECD.append("div")
 	.attr("class", "play-button")
-	.text("PLAY ALL YEARS"); // Remove when Pause is added.
+	.text("PLAY ALL YEARS"); // Remove when Pause is added. NEEDFIX
 
 var buttons = OECD.append("div")
 	.attr("class", "yearsContainer")
@@ -61,6 +64,26 @@ OECD.append("div")
 OECD.append("div")
 	.attr("class", "clearfix");
 
-d3.csv('data/OECD Labour Force Participation Rate by Sex 2001-2012 Linear 20150228a.csv',
+var OECDsvg = OECD.append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv('data/OECD_Labour_Force_Participation_Rate_by_Sex_2001-2012_Tabular_20150301a.csv',
 	function(error, data) {
+		var popData = data.filter(function(element) { return element.Year == year; } );
+		console.log(popData);
+
+//		console.log(d3.keys(data[0]).filter(function(key) { return key !== "Country"; }));
+		color.domain(d3.keys(popData[0]).filter(function(key) { return key !== "Country"; }));
+
+		popData.forEach(function(d) {
+			var y0 = 0;
+			d.sexes = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+			d.total = d.sexes[d.sexes.length - 1].y1;
+			console.log(d);
+		});
+
+//		data.sort(function(a
 	});
