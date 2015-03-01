3ccc58356f64bf6bb20f4b8aca11539d95e2ca53
Jacob Joseph <jjoseph03@mica.edu>
2015-03-01 15:26:32 -0800

    still not working... trying to incorporate bosticks 3885705 bl.ock

--- js/main.js
+++ js/main.js
@@ -18,14 +18,13 @@ var y = d3.scale.ordinal()
 	.rangeRoundBands([0, height], 0.2, 0);
 
 var color = d3.scale.ordinal()
-//	.range(["#EEE", "#BDF", "#FCF", "#EEE"]);
-	.range(["#BDF", "#FCF"]); // NEEDFIX must add 2 more columsn of data and then use 4 colors
+	.range(["#BDF", "#FCF"]);
 
 var xAxis = d3.svg.axis()
 	.scale(x)
 	.orient("top")
-	.ticks(10, "%") // Not sure if this is right since it is 50/50 split NEEDFIX
-	.tickSize(-height); // +/- No clue where this is going to end up... NEEDFIX
+	.ticks(10, "%")
+	.tickSize(-height);
 
 var yAxis = d3.svg.axis()
 	.scale(y)
@@ -77,35 +76,12 @@ var OECDsvg = OECD.append("svg")
 
 d3.csv('data/OECD_Labour_Force_Participation_Rate_by_Sex_2001-2012_Tabular_20150301c.csv',
 	function(error, data) {
-	var popData = data.filter(function(element) { return element.Year == year; } );
+	var popData = data.filter(function(element) { return element.Year == year; } )
+		.sort(function(a, b) { return +b.Male - +a.Male; });
 
 	color.domain(d3.keys(popData[0]).filter(function(key) {
 		return (key !== "Country" && key !== "Year");
 	}));
-
-	var OECDmidpointCompare = function(a, b) {
-		// Must change to 50 - and 50 + after adapting #s to 100% instead of 200% NEEDFIX
-		// can also just use the unemployed value if that happens... NEEDFIX
-		if (+a < +b) {
-			return -1;
-		} else if (+a > +b) {
-			return 1;
-		} else if (+a == +b) {
-			return 0;
-		} else {
-			return NaN;
-		}
-
-		return mp;
-	}
-
-	var OECDmidpoint = function(m, f) {
-		// Must change to 50 - and 50 + after adapting #s to 100% instead of 200% NEEDFIX
-		// can also just use the unemployed value if that happens... NEEDFIX
-		mp = (100 - +m + 100 + +f) / 2.0;
-
-		return mp;
-	}
 
 	popData.forEach(function(d, i) {
 		var x0 = 0;
@@ -117,32 +93,24 @@ d3.csv('data/OECD_Labour_Force_Participation_Rate_by_Sex_2001-2012_Tabular_20150
 				x1: (x0 += +d[name])
 			};
 		});
-		d.midpoint = OECDmidpoint(d.Male, d.Female);
 		d.total = d.sexes[d.sexes.length - 1].x1;
 		console.log(d);
 	});
 
-	// to flip sorting order, reverse b and a on return or just change the sign
-//	popData.sort(function(a, b) { return b.order - a.order; });
-//	popData.sort(function(a, b) { return OECDmidpointCompare(a.midpoint, b.midpoint); });
-
-	x.domain([0, d3.max(data, function(element) { return element.Year == year; })]);
-	y.domain(data.map(function(d) { return d.Country; }));
+//	x.domain([0, d3.max(data, function(element) { return element.Year == year; })]);
+	y.domain(data.map(function(d) { return keys(d); }));
 
 	var countries = OECDsvg.selectAll("g.countries")
-		.sort(function(a, b) { return OECDmidpointCompare(a.midpoint, b.midpoint); })
 		.data(popData, keys)
 		.enter().append("g")
 		.attr("class", "countries")
 		.attr("id", function(d) { return "c" + d.Country.replace(" ",""); })
 		.attr("transform", function(d) { return "translate(0," + y(d.Country) + ")"; });
-//		.attr("transform", function(d) { return "translate(0,15)"; });
 
 	var bars = countries.selectAll("rect")
 		.data(function(d) { return d.sexes; })
 		.enter().append("rect")
 		.attr("x", function(d) { return x(d.x0); })
-//		.attr("y", function(d) { console.log(d); return y(d.Country);  })
 		.attr("height", y.rangeBand())
 		.attr("width", function(d) { return x(d.x1) - x(d.x0); })
 		.style("fill", function(d) { return color(d.name); });
