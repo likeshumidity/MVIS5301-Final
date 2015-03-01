053504bf2c73f2706bb9cd0f2a9f6e04ba141177
Jacob Joseph <jjoseph03@mica.edu>
2015-03-01 10:09:24 -0800

    changed data from 100 * % to %, and begain svg axes

--- js/main.js
+++ js/main.js
@@ -18,11 +18,12 @@ var y = d3.scale.ordinal()
 	.rangeBands([0, height], 0.2, 0);
 
 var color = d3.scale.ordinal()
-	.range(["#EEE", "#BDF", "#FCF", "#EEE"]);
+//	.range(["#EEE", "#BDF", "#FCF", "#EEE"]);
+	.range(["#BDF", "#FCF"]); // NEEDFIX must add 2 more columsn of data and then use 4 colors
 
 var xAxis = d3.svg.axis()
 	.scale(x)
-	.orient("bottom")
+	.orient("top")
 	.ticks(10, "%") // Not sure if this is right since it is 50/50 split NEEDFIX
 	.tickSize(-height); // No clue where this is going to end up... NEEDFIX
 
@@ -70,20 +71,42 @@ var OECDsvg = OECD.append("svg")
 	.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv('data/OECD_Labour_Force_Participation_Rate_by_Sex_2001-2012_Tabular_20150301a.csv',
+d3.csv('data/OECD_Labour_Force_Participation_Rate_by_Sex_2001-2012_Tabular_20150301b.csv',
 	function(error, data) {
-		var popData = data.filter(function(element) { return element.Year == year; } );
-		console.log(popData);
+	var popData = data.filter(function(element) { return element.Year == year; } );
+	console.log(popData);
 
-//		console.log(d3.keys(data[0]).filter(function(key) { return key !== "Country"; }));
-		color.domain(d3.keys(popData[0]).filter(function(key) { return key !== "Country"; }));
+	color.domain(d3.keys(popData[0]).filter(function(key) {
+		return (key !== "Country" && key !== "Year");
+	}));
 
-		popData.forEach(function(d) {
-			var y0 = 0;
-			d.sexes = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
-			d.total = d.sexes[d.sexes.length - 1].y1;
-			console.log(d);
+	popData.forEach(function(d) {
+		var y0 = 0;
+		d.sexes = color.domain().map(function(name) {
+			return {
+				name: name,
+				y0: y0,
+				y1: (y0 += +d[name])
+			};
 		});
-
-//		data.sort(function(a
+		d.total = d.sexes[d.sexes.length - 1].y1;
+		console.log(d);
 	});
+
+	var OECDmidpoint = function(countryYear) {
+		// Must change to 50 - and 50 + after adapting #s to 100% instead of 200% NEEDFIX
+		// can also just use the unemployed value if that happens... NEEDFIX
+		return (100 - countryYear.Male + 100 + countryYear.Female) / 2.0;
+	}
+
+	// to flip sorting order, reverse b and a on return or just change the sign
+	popData.sort(function(a, b) { return OECDmidpoint(b) - OECDmidpoint(a); });
+
+	x.domain([0, d3.max(popData, function(d) { return d.total; })]);
+	y.domain(data.map(function(d) { return d.Country; }));
+
+	OECDsvg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+});
